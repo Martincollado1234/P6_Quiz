a57c170f80882b80e09198fb2e37e601cbc2f62c
Jose Miguel Balanya <jm.balanya@alumnos.upm.es>
2018-05-21 19:24:44 +0200

    P6_Quiz

---
+++ models/index.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+// Load ORM
+const Sequelize = require('sequelize');
+
+
+// To use SQLite data base:
+//    DATABASE_URL = sqlite:quiz.sqlite
+// To use  Heroku Postgres data base:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
+
+const url = process.env.DATABASE_URL || "sqlite:quizzes.sqlite";
+
+const sequelize = new Sequelize(url);
+
+// Import the definition of the Quiz Table from quiz.js
+sequelize.import(path.join(__dirname, 'quiz'));
+
+// Session
+sequelize.import(path.join(__dirname,'session'));
+
+// Create tables
+sequelize.sync()
+.then(() => sequelize.models.quiz.count())
+.then(count => {
+    if (!count) {
+        return sequelize.models.quiz.bulkCreate([
+            { question: "Capital de Italia", answer: "Roma" },
+            { question: "Capital de España", answer: "Madrid" },
+            { question: "Capital de Francia", answer: "París" },
+            { question: "Capital de Portugal", answer: "Lisboa" }
+        ]);
+    }
+})
+.then(() => console.log('Data Bases created successfully'))
+.catch(error => {
+    console.log("Error creating the data base tables:", error);
+    process.exit(1);
+});
+
+
+module.exports = sequelize;
